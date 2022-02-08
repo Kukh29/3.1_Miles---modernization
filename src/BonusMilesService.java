9ca85cbf977b27ea00ce899d6c9232f90cd81ba5
kukh29 <kukh29@gmail.com>
2022-02-08 13:18:48 +0300

    v.1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+        int price = 10_000; // сколько стоит 1 билет
+        int spend = 20; // 1 миля начисляется за 20 рублей
+        int miles = price/spend;
+        return miles;
+    }
+}
